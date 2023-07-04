1d9238ef63133a271a5a835f479d8c2bdd856fbc
DanielCamposDevX <daniel.campos.dev3@gmail.com>
2023-07-04 14:50:34 -0300

    Post to SignIn

--- src/app.js
+++ src/app.js
@@ -1,3 +1,7 @@
+/// Package Imports ///
+import Joi from "joi";
+
+
 /// Imports API Connection & Security
 import express from "express";
 import cors from "cors";
@@ -27,12 +31,36 @@ try {
 catch (error) {
     console.log(error)
 }
+////Joi Schemas ////
+const userschema = Joi.object({
+    email: Joi.string().email().required(),
+    password: Joi.string().min(3).required()
+  });
+
 
 //// ENDPOINTS ////
 
-
-
-
+app.post("/signin", async (req, res) => {
+    const { email, password } = req.body;
+    const validation = userschema.validate({ email: email, password: password })
+    try {
+        const user = await db.collection('users').findOne({ email: email });
+        if (!user) {
+            return res.status(404).send('E-mail not found!');
+        }
+        else {
+            if (user.password === password) {
+                return res.status(200).send('TOKEN AQUI');//// Colocar um token /////
+            }
+            else {
+                return res.status(401).send('Wrong password');
+            }
+        }
+    }
+    catch (error) {
+        return res.status(500).send(error);
+     }
+})
 
 
 
