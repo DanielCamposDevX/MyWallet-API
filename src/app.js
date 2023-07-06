5b3b91f7ea55e9fcc36e15985491a0ad0af9f464
DanielCamposDevX <daniel.campos.dev3@gmail.com>
2023-07-05 21:53:50 -0300

    chore: adjust code for deploy on render

--- src/app.js
+++ src/app.js
@@ -136,7 +136,7 @@ app.post("/logoff", async (req, res) => {
     try {
         await db.collection("sessions").deleteOne({ token: token });
     }
-    catch(error){
+    catch (error) {
         return res.status(500).send(error);
     }
 })
@@ -151,5 +151,7 @@ app.post("/logoff", async (req, res) => {
 
 
 /// PORT Declaration and Listen
-const PORT = 5000;
-app.listen(PORT, () => console.log(`Rodando na porta ${PORT}`))
+const port = process.env.PORT || 5000
+app.listen(port, () => {
+    console.log(`Servidor rodando na porta ${port}`)
+})
