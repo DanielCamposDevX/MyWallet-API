99178070fd910fe37eba9b2f12c94fbab7cdc543
DanielCamposDevX <daniel.campos.dev3@gmail.com>
2023-07-11 09:03:58 -0300

    Controllers

--- src/app.js
+++ src/app.js
@@ -1,10 +1,3 @@
-/// Package Imports ///
-import Joi from "joi";
-import bcrypt from "bcrypt";
-import { v4 as uuid } from "uuid";
-import dayjs from "dayjs";
-
-
 /// Imports API Connection & Security
 import express from "express";
 import cors from "cors";
@@ -21,139 +14,33 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-
+/// Controllers ///
+import { signin, signup, logoff } from "./controllers/users.js";
+import { gettransactions, posttransactions } from "./controllers/transactions.js";
 
 
 /// Database Connection ///
 const mongoClient = new MongoClient(process.env.DATABASE_URL);
-let db;
+
 try {
     await mongoClient.connect()
-    db = mongoClient.db()
 }
 catch (error) {
     console.log(error)
 }
-////Joi Schemas ////
-const userschema = Joi.object({
-    email: Joi.string().email().required(),
-    password: Joi.string().min(3).required(),
-});
-const transactionschema = Joi.object({
-    value: Joi.number().positive().required(),
-    description: Joi.string().required(),
-    type: Joi.string().valid("entrada", "saida")
-})
+export const db = mongoClient.db()
 
 //// ENDPOINTS ////
 
+app.get(("/transactions"), gettransactions)
 
-app.get(("/transactions"), async (req, res) => {
-    const { authorization } = req.headers;
-    const token = authorization?.replace("Bearer ", "");
-    try {
-        if (!token) { return res.status(401).send("Token error") }
-        const session = await db.collection("sessions").findOne({ token });
-        if (!session) { return res.status(401).send("Session Expired") }
-        const transactions = await db.collection("transactions").find({ sessionId: session.userId }).toArray();
-        return res.send(transactions)
-    }
-    catch (error) {
-        res.status(500).send(error.message);
-    }
-})
+app.post(("/transactions"), posttransactions)
 
+app.post("/signin", signin)
 
-app.post(("/transactions"), async (req, res) => {
-    const { authorization } = req.headers;
-    const token = authorization?.replace("Bearer ", "");
-    try {
-        if (!token) { return res.status(401).send("Token Error") }
-        const session = await db.collection("sessions").findOne({ token });
-        if (!session) { return res.status(401).send("Session Expired") }
+app.post("/signup", signup)
 
-        const { value, description, type } = req.body;
-        const data = { value, description, type };
-        const validation = transactionschema.validate(data, { abortEarly: false });
-        if (validation.error) {
-            const errors = validation.error.details.map((detail) => detail.message);
-            return res.status(422).send(errors);
-        }
-        const transaction = { data, sessionId: session.userId, date: dayjs().format('DD/MM') };
-        await db.collection("transactions").insertOne(transaction);
-        return res.status(201).send("Created")
-    }
-    catch (error) {
-        res.status(500).send(error.message);
-    }
-})
-
-app.post("/signin", async (req, res) => {
-    const { email, password } = req.body;
-    const validation = userschema.validate({ email, password }, { abortEarly: false })
-    if (validation.error) {
-        const errors = validation.error.details.map((detail) => detail.message);
-        return res.status(422).send(errors);
-    }
-    try {
-        const user = await db.collection('users').findOne({ email });
-        if (!user) { return res.status(404).send('E-mail not found!'); }
-        const compare = bcrypt.compareSync(password, user.password);
-        if (!compare) { return res.status(401).send('Wrong password'); }
-
-        const exists = await db.collection("sessions").findOne({ userId: user._id });
-        if (exists) { return res.status(200).send({ token: exists.token, name: user.name }) };
-        const token = uuid();
-        await db.collection("sessions").insertOne({ userId: user._id, token });
-        return res.status(200).send({ token, name: user.name });
-    }
-    catch (error) {
-        return res.status(500).send(error.message);
-    }
-})
-
-app.post("/signup", async (req, res) => {
-    const { name, email, password } = req.body;
-    const validation = userschema.validate({ email, password }, { abortEarly: false })
-    if (validation.error) {
-        const errors = validation.error.details.map((detail) => detail.message);
-        res.status(422).send(errors)
-    }
-    try {
-        const user = await db.collection('users').findOne({ email: email });
-        if (user) { return res.status(409).send('Email already in use'); }
-
-        const hash = bcrypt.hashSync(password, 10);
-        await db.collection('users').insertOne({ name, email, password: hash });
-        return res.status(201).send('created');
-    }
-    catch (error) {
-        return res.status(500).send(error.message);
-    }
-})
-
-app.post("/logoff", async (req, res) => {
-    const { authorization } = req.headers;
-    const token = authorization?.replace("Bearer ", "");
-    console.log(token)
-    try {
-        const user = await db.collection("sessions").findOne({ token: token });
-        if(!user){ return res.status(404).send("User Error")};
-        await db.collection("sessions").deleteOne(user);
-        return res.status(200).send("logged off");
-    }
-    catch (error) {
-        return res.status(500).send(error.message);
-    }
-})
-
-
-
-
-
-
-
-
+app.post("/logoff", logoff)
 
 
 /// PORT Declaration and Listen
---
+++ src/controllers/transactions.js
@@ -0,0 +1,55 @@
+/// Package Imports ///
+import Joi from "joi";
+import dayjs from "dayjs";
+
+/// Configs imports ///
+import { db } from "../app.js";
+
+
+/// Joi Schema ///
+const transactionschema = Joi.object({
+    value: Joi.number().positive().required(),
+    description: Joi.string().required(),
+    type: Joi.string().valid("entrada", "saida")
+})
+
+/// ENDPOINTS ///
+
+export async function gettransactions(req, res) {
+    const { authorization } = req.headers;
+    const token = authorization?.replace("Bearer ", "");
+    try {
+        if (!token) { return res.status(401).send("Token error") }
+        const session = await db.collection("sessions").findOne({ token });
+        if (!session) { return res.status(401).send("Session Expired") }
+        const transactions = await db.collection("transactions").find({ sessionId: session.userId }).toArray();
+        return res.send(transactions)
+    }
+    catch (error) {
+        res.status(500).send(error.message);
+    }
+}
+
+export async function posttransactions(req, res) {
+    const { authorization } = req.headers;
+    const token = authorization?.replace("Bearer ", "");
+    try {
+        if (!token) { return res.status(401).send("Token Error") }
+        const session = await db.collection("sessions").findOne({ token });
+        if (!session) { return res.status(401).send("Session Expired") }
+
+        const { value, description, type } = req.body;
+        const data = { value, description, type };
+        const validation = transactionschema.validate(data, { abortEarly: false });
+        if (validation.error) {
+            const errors = validation.error.details.map((detail) => detail.message);
+            return res.status(422).send(errors);
+        }
+        const transaction = { data, sessionId: session.userId, date: dayjs().format('DD/MM') };
+        await db.collection("transactions").insertOne(transaction);
+        return res.status(201).send("Created")
+    }
+    catch (error) {
+        res.status(500).send(error.message);
+    }
+}
