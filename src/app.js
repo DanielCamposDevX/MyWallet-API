b4ba0221d51656c97ec55ab91de1d13c67bab09a
DanielCamposDevX <daniel.campos.dev3@gmail.com>
2023-07-11 11:59:54 -0300

    Better Code Structure

--- src/app.js
+++ src/app.js
@@ -1,21 +1,14 @@
 /// Imports API Connection & Security
 import express from "express";
 import cors from "cors";
+import router from "./routes/index.routes.js";
 
-/// Imports Database & Security
-import dotenv from "dotenv";
-dotenv.config();
-
-/// API Connection ///
 const app = express();
+
 app.use(express.json());
 app.use(cors());
-
-/// Router ///
-import router from "./routes/index.routes.js";
 app.use(router)
 
-/// PORT Declaration and Listen
 const port = process.env.PORT || 5000
 app.listen(port, () => {
     console.log(`Servidor rodando na porta ${port}`)
